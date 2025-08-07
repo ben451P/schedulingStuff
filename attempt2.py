aac9c0785fef87ddeea0c1a0f4c84db002b02359
benL25 <148346195+benL25@users.noreply.github.com>
2025-08-07 08:59:58 -0400

    Working rotation, just backwards

--- attempt2.py
+++ attempt2.py
@@ -115,40 +115,50 @@ class Scheduler:
 
         prev_state = []
         for i,j in enumerate(prev_availability):
-            if j:prev_state.append(i+1)
+            if j:prev_state.append(i)
 
         for row in range(len(self.schedule)):
             availability,num_avail = self.available_guards(minutes_to_time(time))
 
             #this is a rotation #####
             new_state = prev_state.copy()
+            print(new_state)
 
-            # temp = new_state.copy() + [-1] * (len(ROTATION_CYCLE) - len(new_state))
-            # importance_index = {name: i for i, name in enumerate(STATION_IMPORTANCE_DESCENDING)}
-            # reordered = [temp[importance_index[station]] for station in ROTATION_CYCLE]
-            # reordered = reordered[:len(reordered) - reordered[::-1].index(-1) - 1]
+            temp = new_state.copy() + [-1] * (len(ROTATION_CYCLE) - len(new_state))
+            importance_index = {name: i for i, name in enumerate(STATION_IMPORTANCE_DESCENDING[::-1])}
+            reordered = [temp[importance_index[station]] for station in ROTATION_CYCLE]
+            while reordered[-1] == -1:
+                reordered.pop(-1)
+            print(minutes_to_time(time))
 
-            # temp = reordered[-1]
-            # gap = 1
-            # for i in range(1,len(reordered)):
-            #     if reordered[i] == -1:
-            #         gap += 1
-            #     else:
-            #         reordered[i] = reordered[i-gap]
-            #         gap = 1
-            # reordered[0] = temp
+            temp = reordered[0]
+            gap = 1
+            pointer = 0
+            while pointer < len(reordered) - 1:
+                if reordered[pointer+gap] == -1:
+                    gap += 1
+                else:
+                    reordered[pointer] = reordered[pointer + gap]
+                    pointer += gap
+                    gap = 1
+            reordered[-1] = temp
             
-            # temp = reordered.copy() + [-1] * (len(ROTATION_CYCLE) - len(reordered))
-            # cycle_index = {name: i for i, name in enumerate(ROTATION_CYCLE)}
-            # original_order = [temp[cycle_index[station]] for station in STATION_IMPORTANCE_DESCENDING]
+            temp = reordered.copy() + [-1] * (len(ROTATION_CYCLE) - len(reordered))
+            cycle_index = {name: i for i, name in enumerate(ROTATION_CYCLE)}
+            original_order = [temp[cycle_index[station]] for station in STATION_IMPORTANCE_DESCENDING[::-1]]
 
-            # new_state = original_order[:len(original_order) - original_order[::-1].index(-1) - 1]
-            # print(prev_state,"\n",new_state,"\n\n")
-            new_state.append(new_state.pop(0))
+            while original_order[-1] == -1:
+                original_order.pop(-1)
+            
+            new_state = original_order.copy()
+
+            print(new_state)
             #up to here ####
 
             if prev_availability != availability:
+                print(availability,prev_availability)
                 for i in range(len(availability)):
+                    
                     if not availability[i] and prev_availability[i]:
                         new_state[new_state.index(i)] = -1
                 for i in range(len(availability)):
